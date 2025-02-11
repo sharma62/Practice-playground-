b76374a4ffda11be9b244bd78772073e8a28a49b
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2025-02-11 15:37:20 +0530

    lec 13 completed

--- 8_Adv_JS/lec-13/script.js
+++ 8_Adv_JS/lec-13/script.js
@@ -1,16 +1,21 @@
+/*
+Objective: Understand event bubbling, event delegation, and this keyword in JavaScript.
 
+Requirements:
+
+Explain event bubbling with an example.
+Implement event delegation to handle events on multiple elements.
+Use this keyword to access the context of a function.
+
+*/
 function debounce(func, delay) {
     let timer = delay
-    console.log('debounce',delay)
     return function (){
-        clearTimeout(delay)
+        clearTimeout(dexlay)
         timer = setTimeout(() => {
             handleSearch()
         }, delay);
-        
      }
-
-
 }
 
 
