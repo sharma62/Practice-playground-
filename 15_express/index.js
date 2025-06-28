390d22c8c79fe87d4314ed12cb8f289993a8ac6d
unknown <surajkumarsharma62@gmail.com>
2025-06-28 23:29:53 +0530

    middleware updated

--- 15_express/index.js
+++ 15_express/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const path = require('path');
-const app= express();
-const port = 5050;
+// const express = require('express');
+// const path = require('path');
+// const app= express();
+// const port = 5050;
 
-app.get('/',(req,res)=>{
-    // res.send("hello home page")
-    // res.sendStatus(200)
-    // res.status(200).send("hello home page");
-    // res.status(500).send("server error");
-    // res.json({name:"john",age:30})
-    res.sendFile(path.resolve((__dirname) + '/main.html' ))
-    // res.download(path.resolve((__dirname) + '/main.html'))
+// app.get('/',(req,res)=>{
+//     // res.send("hello home page")
+//     // res.sendStatus(200)
+//     // res.status(200).send("hello home page");
+//     // res.status(500).send("server error");
+//     // res.json({name:"john",age:30})
+//     res.sendFile(path.resolve((__dirname) + '/main.html' ))
+//     // res.download(path.resolve((__dirname) + '/main.html'))
 
-})
+// })
 
 
-app.listen(port,()=>{
-    console.log(`server runing on localhost:${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`server runing on localhost:${port}`)
+// })
--- 15_express/middleWare/middleWere.js
+++ 15_express/middleWare/middleWere.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const app = express();
-
+//Q 1. i am try to call next middleware or last responese after 5 sec but it's not working why
+//why 5 sec -  in 5 sec main user ki kuch loading type dikana chata hu
 const myMiddleware = (req, res, next) => {
-  res.send("Hello from MY-middleware!"); // Send a response directly from the middleware
+  console.log("before next");
   setTimeout(() => {
-    next(); // Call next() after a delay
+    next(); // Call next() after aa delay
   }, 5000); // Simulate a delay of 5 seconds
+  console.log("after next");
   console.log("Middleware executed");
   console.log("Request URL:", req.url);
 };
 
+const newMiddleWare = (req, res, next) => {
+  res.send("Hello from MY-middleware is !"); // Send a response directly from the middleware
+};
 // app.use(myMiddleware); // Use the middleware globally
 
-app.get("/", myMiddleware, (req, res) => {
+app.get("/", myMiddleware, newMiddleWare, (req, res) => {
   res.send("hello home page");
 });
 app.listen(3000, () => {
